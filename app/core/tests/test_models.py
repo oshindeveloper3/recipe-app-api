a8e4be24a02de219a92fb70e8ff26a9d9ecd2d61
oshindeveloper3 <oshindeveloper3@gmail.com>
2020-01-24 10:33:55 -0400

    Added custom user model.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    def test_create_user_with_email_successfull(self):
+        '''Test creating a new user with an email is succesful'''
+        email = 'test@test.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(email, user.email)
+        self.assertTrue(user.check_password(password))
+
+    def test_is_normalized_new_user_email_address(self):
+        '''Test the email for a new user is normalized'''
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_email_invalid(self):
+        '''Test creating user with no email raises error'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(' ', 'test123')
+
+    def test_white_spaces_in_new_user_email(self):
+        '''Test filling email with white spaces raises error'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(' ', 'test123')
+
+    def test_create_new_superuser(self):
+        '''Test creating a new superuser'''
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
